5d305bc1e6a163a9b5fa65f071926fa25765af8a
bogoquiz <contactsaliens@gmail.com>
2015-08-12 12:06:39 -0500

    fix verbs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
     resources :articles do
-      resources :comments
+      resources :comments 
+       
     end
+    resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
